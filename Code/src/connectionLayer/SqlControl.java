b98f9a9f68a0de6a9880d12540df09b0434557a5
razco <rcm1_2@hotmail.com>
2012-12-11 20:27:19 +0300

    necessary imports

--- Code/src/connectionLayer/SqlControl.java
+++ Code/src/connectionLayer/SqlControl.java
@@ -1,5 +1,15 @@
 package connectionLayer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.management.Query;
+
+//******************************************************************
 public class SqlControl {
 
 }
