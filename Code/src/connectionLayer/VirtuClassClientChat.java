0c03a8289a9d30886e542bb33a93d0675390c579
razco <rcm1_2@hotmail.com>
2012-12-12 08:59:01 +0200

    VirtuClassClientChat - main

--- Code/src/connectionLayer/VirtuClassClientChat.java
+++ Code/src/connectionLayer/VirtuClassClientChat.java
@@ -22,12 +22,22 @@ package connectionLayer;
 	  private static boolean closed = false;
 	  
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  public static void main(String[] args) {
 
-	}
+		    // The default port.
+		    int portNumber = 8888;
+		    // The default host.
+		    String host = "10.8.4.110";
 
-
+		    if (args.length < 2) {
+		      System.out
+		          .println("Usage: java MultiThreadChatClient <host> <portNumber>\n"
+		              + "Now using host=" + host + ", portNumber=" + portNumber);
+		    } else {
+		      host = args[0];
+		      portNumber = Integer.valueOf(args[1]).intValue();
+		    }
+	  }
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
