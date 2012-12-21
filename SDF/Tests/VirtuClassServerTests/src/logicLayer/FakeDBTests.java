b85dff7fee9fe142c025365793053254c7ca3b28
razco <rcm1_2@hotmail.com>
2012-12-21 18:45:28 +0300

    added FakeDB tests

---
+++ SDF/Tests/VirtuClassServerTests/src/logicLayer/FakeDBTests.java
@@ -0,0 +1,14 @@
+package logicLayer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FakeDBTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
