25728264c939456c99ed529e1a2f5ecf4ddd4e1f
razco <rcm1_2@hotmail.com>
2012-12-21 20:57:14 +0300

    added signUp option and close socket on each operation end

--- Code/VirtuClassClient/src/logicLayer/Login.java
+++ Code/VirtuClassClient/src/logicLayer/Login.java
@@ -1,5 +1,7 @@
 package logicLayer;
 
+import java.io.IOException;
+
 import javax.swing.JOptionPane;
 
 import connectionLayer.Synchronizer;
@@ -11,13 +13,14 @@ public class Login{
 	public Login(LoginInfo li) throws Exception
 	{
 		this.li=li;
-		_sync = Synchronizer.getSync();
-		boolean connected = _sync.connect(8500);
-		if(!connected)
-			throw new Exception("Connection failed");
+//		_sync = Synchronizer.getSync();
+//		boolean connected = _sync.connect(8500);
+//		if(!connected)
+//			throw new Exception("Connection failed");
 	}
 	
-	public void login(){
+	public void login() throws Exception{
+		reconnect();
 		String username=li.getUsername();
 		char [] cs = li.getPassword();
 		if(username==null || username.length()==0)
@@ -36,7 +39,40 @@ public class Login{
 		_sync.sendData("0"+username);
 		_sync.sendData("0"+pw);
 		_sync.getData();
-		_sync.getData();
+		_sync.disconnect();
+		
 	}
 	
+	
+	public void signUp() throws Exception//for now it's almost like login. But later it will change
+	{
+		reconnect();
+		String username=li.getUsername();
+		char [] cs = li.getPassword();
+		if(username==null || username.length()==0)
+		{
+			JOptionPane.showMessageDialog(null, "Please enter username");
+			return;
+		}
+
+		String pw = String.copyValueOf(cs);
+		if(pw==null || pw.length()==0)
+		{
+			JOptionPane.showMessageDialog(null, "Please enter password");
+			return;
+		}
+		
+		_sync.sendData("1"+username);
+		_sync.sendData("1"+pw);
+		_sync.getData();
+		_sync.disconnect();
+	}
+	
+	private void reconnect()throws Exception
+	{
+		_sync = Synchronizer.getSync();
+		boolean connected = _sync.connect(8500);
+		if(!connected)
+			throw new Exception("Connection failed");
+	}
 }
