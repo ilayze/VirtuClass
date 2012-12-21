2203813c10d1bcd42e7ca799f8c42ff9fd14e3c1
razco <rcm1_2@hotmail.com>
2012-12-21 18:51:04 +0300

    green - fake signUp

--- SDF/Tests/VirtuClassServerTests/src/logicLayer/FakeDBTests.java
+++ SDF/Tests/VirtuClassServerTests/src/logicLayer/FakeDBTests.java
@@ -11,6 +11,8 @@ public class FakeDBTests {
 	public void test() {
 		
 		assertEquals(-1, db.signIn("wrongUsr", "1234"));
+		assertEquals(true, db.signUp("newUsr1", "12345",0));
+		//assertEquals(0, db.signIn("newUsr1", "12345"));
 	}
 
 }
