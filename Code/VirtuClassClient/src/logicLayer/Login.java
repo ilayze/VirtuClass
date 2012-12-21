f4d82d95973fa82d8e97db95968c9e78e36c17d1
razco <rcm1_2@hotmail.com>
2012-12-21 19:53:29 +0300

    added simple header for login requests

--- Code/VirtuClassClient/src/logicLayer/Login.java
+++ Code/VirtuClassClient/src/logicLayer/Login.java
@@ -33,8 +33,8 @@ public class Login{
 			return;
 		}
 		
-		_sync.sendData(username);
-		_sync.sendData(pw);
+		_sync.sendData("0"+username);
+		_sync.sendData("0"+pw);
 		_sync.getData();
 		_sync.getData();
 	}
