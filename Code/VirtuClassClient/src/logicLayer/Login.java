b22e32f516023e107ecb51487008ddd5700e31cd
razco <rcm1_2@hotmail.com>
2012-12-21 15:37:07 +0300

    added getPw, getUser functions using java's function pointers

--- Code/VirtuClassClient/src/logicLayer/Login.java
+++ Code/VirtuClassClient/src/logicLayer/Login.java
@@ -2,23 +2,35 @@ package logicLayer;
 
 import connectionLayer.Synchronizer;
 
-public class Login {
+public class Login{
 	Synchronizer _sync;
+	LoginInfo li;
 	
-	public Login() throws Exception
+	public Login(LoginInfo li) throws Exception
 	{
+		this.li=li;
 		_sync = Synchronizer.getSync();
 		boolean connected = _sync.connect(8500);
 		if(!connected)
 			throw new Exception("Connection failed");
 	}
 	
-	public void login(String username,String password){
+	public void login(String username,char[] cs){
 		_sync.sendData(username);
-		_sync.sendData(password);
+		String pw = String.copyValueOf(cs);
+		_sync.sendData(pw);
 		_sync.getData();
 		_sync.getData();
-		 
+	}
+	
+	String getUsername()
+	{
+		return li.getUsername();
+	}
+	
+	char[] getPassword()
+	{
+		return li.getPassword();
 	}
 	
 }
