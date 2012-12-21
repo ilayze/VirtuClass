4a09e3217f769d25b4793e2682a788dc8abd8f22
razco <rcm1_2@hotmail.com>
2012-12-21 19:38:30 +0300

    "advanced" StubDB

--- SDF/Tests/VirtuClassServerTests/src/logicLayer/StubDB.java
+++ SDF/Tests/VirtuClassServerTests/src/logicLayer/StubDB.java
@@ -2,12 +2,12 @@ package logicLayer;
 
 
 public class StubDB {
-	
+
 	private static StubDB db=null;
-	
+
 	private StubDB()
 	{
-		
+
 	}
 	public static StubDB getDB()
 	{
@@ -17,32 +17,40 @@ public class StubDB {
 		}
 		return db;
 	}
-	
+
 	public boolean signUp(String username, String password)
 	{
-		
+		if(username!=null && password!=null && username.length()>0 && password.length()>0)
+			return true;
 		return false;
 	}
 	public int signIn(String username)
 	{
-
+		if(username!=null && username.length()>0)
+			return 0;
 		return -1;
-		
+
 	}
 	public boolean deleteUser(String username)
 	{
+		if(username!=null && username.length()>0)
+			return true;
 		return false;
-		
+
 	}
-	
+
 	public boolean addClass(String classname,String classcreator)
 	{
+		if(classname!=null && classcreator!=null && classname.length()>0 && classcreator.length()>0)
+			return true;
 		return false;
-		
+
 	}
 	public boolean deleteClass(String classname)
 	{
+		if(classname!=null && classname.length()>0)
+			return true;
 		return false;
-		
+
 	}
 }
