fa524845b871b0f19d03b7ad33c3b721d8518508
razco <rcm1_2@hotmail.com>
2012-12-21 18:43:58 +0300

    added signUp,signIn functionality.

--- SDF/Tests/VirtuClassServerTests/src/logicLayer/FakeDB.java
+++ SDF/Tests/VirtuClassServerTests/src/logicLayer/FakeDB.java
@@ -1,11 +1,12 @@
 package logicLayer;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 
 
 public class FakeDB {
-	private static LinkedList <String> list=null;
+	private static LinkedList <FakeUsrData> list=null;
 	private static FakeDB db=null;
 	
 	private FakeDB()
@@ -16,7 +17,7 @@ public class FakeDB {
 	{
 		if(db==null)
 		{
-			list = new LinkedList<String>();
+			list = new LinkedList<FakeUsrData>();
 			db = new FakeDB();
 		}
 		return db;
@@ -24,11 +25,28 @@ public class FakeDB {
 	
 	public boolean signUp(String username, String password, int permission)
 	{
+		ListIterator<FakeUsrData> itr;
 		
-		return false;
+		for(itr = list.listIterator(); itr.hasNext();)
+			if(itr.next().equals(username))
+				return false;	
+		
+
+		FakeUsrData newUsr = new FakeUsrData(username, password, permission);
+		list.add(newUsr);
+		return true;
 	}
-	public int signIn(String username)
+	public int signIn(String username, String password)
 	{
+		FakeUsrData tmpUsr = new FakeUsrData(username, password, 0);
+		ListIterator<FakeUsrData> itr;
+		for(itr = list.listIterator(); itr.hasNext();)
+		{
+			if(itr.next().equals(tmpUsr))
+			{
+				return (itr.nextIndex()-1);
+			}
+		}
 		return -1;
 		
 	}
