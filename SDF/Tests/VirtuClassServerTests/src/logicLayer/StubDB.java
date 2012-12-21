fd51ed57c450657612e6d6f0728470206b0a7448
razco <rcm1_2@hotmail.com>
2012-12-21 17:39:10 +0300

    Stub database

---
+++ SDF/Tests/VirtuClassServerTests/src/logicLayer/StubDB.java
@@ -0,0 +1,46 @@
+package logicLayer;
+
+public class StubDB {
+	
+	private static StubDB db=null;
+	
+	private StubDB()
+	{
+		
+	}
+	public static StubDB getDB()
+	{
+		if(db==null)
+		{
+			db = new StubDB();
+		}
+		return db;
+	}
+	
+	public boolean signUp(String username, String password, int permission)
+	{
+		
+		return false;
+	}
+	public int signIn(String username)
+	{
+		return -1;
+		
+	}
+	public boolean deleteUser(String username)
+	{
+		return false;
+		
+	}
+	
+	public boolean addClass(String classname,String classcreator)
+	{
+		return false;
+		
+	}
+	public boolean deleteClass(String classname)
+	{
+		return false;
+		
+	}
+}
