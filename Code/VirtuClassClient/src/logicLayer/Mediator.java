fb065872c2d25a3eb3b40355b0128045279cb17a
razco <rcm1_2@hotmail.com>
2012-12-26 13:05:05 +0200

    handles chat

--- Code/VirtuClassClient/src/logicLayer/Mediator.java
+++ Code/VirtuClassClient/src/logicLayer/Mediator.java
@@ -1,10 +1,18 @@
 package logicLayer;
 
+//import guiLayer.EditorFrame;
+import guiLayer.EditorFrame;
+import guiLayer.EditorFrame2;
 import guiLayer.LoginFrame;
 
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JOptionPane;
 
 
 
@@ -25,22 +33,93 @@ public class Mediator{
 			{
 				try {
 					
-					l.signUp();
+					boolean isOk = l.signUp();
+					if(!isOk)
+					{
+						JOptionPane.showMessageDialog(null, "Username already exist.", "Choose a different username", JOptionPane.ERROR_MESSAGE);
+					}
 				} catch (Exception e1) {
-					e1.printStackTrace();
+					System.out.println(e1.getMessage());
 				}
 			}
 			else if(e.getActionCommand()==LoginFrame.LOGIN_CMD)
 			{
 				try {
 		
-					l.login();
+					boolean isOk = l.login();
+					if(!isOk)
+					{
+						JOptionPane.showMessageDialog(null, "Wrong username / password", "Invalid user", JOptionPane.ERROR_MESSAGE);
+					}
+					else
+					{
+// need to return a signal, this signal will make the mediator to open OpenClassMediator
+						EventQueue.invokeLater(new Runnable() {
+							public void run() {
+								try {
+									l.forceExit();
+									OpenClassMediator op = new OpenClassMediator();
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+						});
+						
+					}
+
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					//e1.printStackTrace();
+					System.out.println(e1.getMessage());
 				}
 				
 				 
+				
+			}
+		}
+		
+	}
+	
+	private class OpenClassMediator implements ActionListener
+	{
+		OpenClass op;
+		private OpenClassMediator thisClass;
+
+		public OpenClassMediator() throws InvocationTargetException, InterruptedException
+		{
+
+			thisClass=this;
+			System.out.println("in open class mediator");
+			
+				System.out.println("alala");
+
+				
+				EventQueue.invokeLater(new Runnable() {
+				public void run() {
+						try {
+							EditorFrame2 frame = new EditorFrame2(thisClass);
+							frame.setVisible(true);
+							op = new OpenClass(frame.ei);
+							Thread t1 = new Thread(op);
+							t1.start();
+							
+								
+							
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}});
+			
+
+			//need to open 2 threads here for read/write
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getActionCommand()==EditorFrame2.SEND_CMD)
+			{
+				synchronized (op) { 
+					op.notifyAll();
+				}
 				
 			}
 		}
@@ -58,7 +137,8 @@ public class Mediator{
 				try {
 					LoginMediator m = new LoginMediator();
 					LoginFrame frame = new LoginFrame(m);
-					m.l = new Login(frame.li);
+					WindowEvent wev = new WindowEvent(frame, WindowEvent.WINDOW_CLOSING);
+					m.l = new Login(frame.li,wev);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
