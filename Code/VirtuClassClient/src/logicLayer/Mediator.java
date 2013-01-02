e67e20ba58d27b4b6764162dbb5198ef85d72172
razco <rcm1_2@hotmail.com>
2013-01-02 13:55:03 +0200

    reduced Cyclomatic Complexity by separating to classes

--- Code/VirtuClassClient/src/logicLayer/Mediator.java
+++ Code/VirtuClassClient/src/logicLayer/Mediator.java
@@ -1,35 +1,85 @@
 package logicLayer;
 
 //import guiLayer.EditorFrame;
-import guiLayer.EditorFrame;
 import guiLayer.EditorFrame2;
 import guiLayer.LoginFrame;
 
 import java.awt.EventQueue;
-import java.awt.Toolkit;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JOptionPane;
 
 
 
-public class Mediator{
+
+public class Mediator extends Thread{
 	
 	
-	private class LoginMediator implements ActionListener{
+	private class LoginScreenMediator implements ActionListener{
 	
 		Login l;
+		LoginMediator lm;
+		SignUpMediator sm;
+		
+		public LoginScreenMediator()
+		{
+			lm = new LoginMediator();
+			sm = new SignUpMediator();
+		}
 		
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if(e.getActionCommand()==LoginFrame.EXIT_CMD)
-			{
 				System.exit(0);
-			}
 			else if(e.getActionCommand()==LoginFrame.SIGNUP_CMD)
+				sm.signUpPressed();
+			else if(e.getActionCommand()==LoginFrame.LOGIN_CMD)
+				lm.loginPressed();
+		}
+		
+		private class LoginMediator
+		{
+			public void loginPressed()
+			{
+				try {
+					
+					boolean isOk = l.login();
+					if(!isOk)
+					{
+						JOptionPane.showMessageDialog(null, "Wrong username / password", "Invalid user", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+					loginSucceed();
+				} catch (Exception e1) {
+					System.out.println(e1.getMessage());
+				}
+			}
+			
+			
+			private void loginSucceed()
+			{
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							l.forceExit();
+							new Thread(new OpenClassMediator()).start();
+							//OpenClassMediator op = new OpenClassMediator();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+
+			}
+			
+		}
+		
+		private class SignUpMediator
+		{
+			public void signUpPressed()
 			{
 				try {
 					
@@ -42,76 +92,18 @@ public class Mediator{
 					System.out.println(e1.getMessage());
 				}
 			}
-			else if(e.getActionCommand()==LoginFrame.LOGIN_CMD)
-			{
-				try {
-		
-					boolean isOk = l.login();
-					if(!isOk)
-					{
-						JOptionPane.showMessageDialog(null, "Wrong username / password", "Invalid user", JOptionPane.ERROR_MESSAGE);
-					}
-					else
-					{
-// need to return a signal, this signal will make the mediator to open OpenClassMediator
-						EventQueue.invokeLater(new Runnable() {
-							public void run() {
-								try {
-									l.forceExit();
-									OpenClassMediator op = new OpenClassMediator();
-								} catch (Exception e) {
-									e.printStackTrace();
-								}
-							}
-						});
-						
-					}
-
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					//e1.printStackTrace();
-					System.out.println(e1.getMessage());
-				}
-				
-				 
-				
-			}
 		}
 		
 	}
 	
-	private class OpenClassMediator implements ActionListener
+	private class OpenClassMediator implements ActionListener, Runnable
 	{
 		OpenClass op;
 		private OpenClassMediator thisClass;
 
-		public OpenClassMediator() throws InvocationTargetException, InterruptedException
+		public OpenClassMediator()
 		{
-
 			thisClass=this;
-			System.out.println("in open class mediator");
-			
-				System.out.println("alala");
-
-				
-				EventQueue.invokeLater(new Runnable() {
-				public void run() {
-						try {
-							EditorFrame2 frame = new EditorFrame2(thisClass);
-							frame.setVisible(true);
-							op = new OpenClass(frame.ei);
-							Thread t1 = new Thread(op);
-							t1.start();
-							
-								
-							
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}});
-			
-
-			//need to open 2 threads here for read/write
 		}
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -122,6 +114,24 @@ public class Mediator{
 				}
 				
 			}
+		}
+		@Override
+		public void run() {
+			EventQueue.invokeLater(new Runnable() {
+			public void run() {
+					try {
+						EditorFrame2 frame = new EditorFrame2(thisClass);
+						frame.setVisible(true);
+						op = new OpenClass(frame.ei);
+						Thread t1 = new Thread(op);
+						t1.start();
+						
+							
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}});
 		}
 		
 	}
@@ -135,7 +145,7 @@ public class Mediator{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LoginMediator m = new LoginMediator();
+					LoginScreenMediator m = new LoginScreenMediator();
 					LoginFrame frame = new LoginFrame(m);
 					WindowEvent wev = new WindowEvent(frame, WindowEvent.WINDOW_CLOSING);
 					m.l = new Login(frame.li,wev);
@@ -150,9 +160,8 @@ public class Mediator{
 	
 	
 	public static void main(String[] args) {//init Mediator
-		Mediator m = new Mediator();
+		
+		new Thread(new Mediator());//Mediator m = new Mediator();
 	}
-
-
 
 }
