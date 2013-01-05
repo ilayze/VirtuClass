085d699bb029ba681ca4087115ac18d1032262bd
razco <rcm1_2@hotmail.com>
2013-01-05 11:57:31 +0300

    added extra indentation

--- Code/VirtuClassClient/src/logicLayer/Mediator.java
+++ Code/VirtuClassClient/src/logicLayer/Mediator.java
@@ -1,6 +1,5 @@
 package logicLayer;
 
-//import guiLayer.EditorFrame;
 import guiLayer.EditorFrame;
 import guiLayer.LoginFrame;
 
@@ -26,13 +25,13 @@ public class Mediator extends Thread{
 		LoginMediator lm;
 		SignUpMediator sm;
 		Semaphore loginKey = new Semaphore(1);
-		
+
 		public LoginScreenMediator()
 		{
 			lm = new LoginMediator();
 			sm = new SignUpMediator();
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if(e.getActionCommand()==LoginFrame.EXIT_CMD)
@@ -46,110 +45,109 @@ public class Mediator extends Thread{
 		private class LoginMediator implements Runnable
 		{
 			
-			public LoginMediator()
-			{
-				
-			}
-			private LoginMediator(Semaphore obj)
-			{
-				loginKey=obj;
-			}
-			
-			public void loginPressed()
-			{
-				if(loginKey.tryAcquire())
+				public LoginMediator()
 				{
-					new Thread(new LoginMediator(loginKey)).start();
+					
 				}
-				return;
+				private LoginMediator(Semaphore obj)
+				{
+					loginKey=obj;
+				}
 				
-
-			}
-			
-			
-			private void loginSucceed()
-			{
-				EventQueue.invokeLater(new Runnable() {
-					public void run() {
-						try {
-							l.forceExit();
-							new Thread(new OpenClassMediator()).start();
-							//OpenClassMediator op = new OpenClassMediator();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
+				public void loginPressed()
+				{
+					if(loginKey.tryAcquire())
+					{
+						new Thread(new LoginMediator(loginKey)).start();
 					}
-				});
-
-			}
-
-			private void tryToLogin() throws Exception
-			{
-				boolean isOk = l.sendRequest(0);//1=signUp, 0 = login
-				if(!isOk)
-				{
-					JOptionPane.showMessageDialog(null, "Wrong username / password", "Invalid user", JOptionPane.ERROR_MESSAGE);
 					return;
+					
+	
 				}
-				loginSucceed();
-			}
 			
-			
-			@Override
-			public void run() {
-				try {
-					tryToLogin();
-				} catch (Exception e1) {
-					System.out.println(e1.getMessage());
-				}
-				finally
+				
+				private void loginSucceed()
 				{
-					loginKey.release();
-				}		
-			}
-			
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								l.forceExit();
+								new Thread(new OpenClassMediator()).start();
+								//OpenClassMediator op = new OpenClassMediator();
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+	
+				}
+	
+				private void tryToLogin() throws Exception
+				{
+					boolean isOk = l.sendRequest(0);//1=signUp, 0 = login
+					if(!isOk)
+					{
+						JOptionPane.showMessageDialog(null, "Wrong username / password", "Invalid user", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+					loginSucceed();
+				}
+				
+				
+				@Override
+				public void run() {
+					try {
+						tryToLogin();
+					} catch (Exception e1) {
+						System.out.println(e1.getMessage());
+					}
+					finally
+					{
+						loginKey.release();
+					}		
+				}	
 		}
 		
 		private class SignUpMediator implements Runnable
 		{
-			private SignUpMediator(Semaphore obj) {
-				loginKey=obj;	
-			}
-
-			public SignUpMediator()
-			{
-			}
-			
-			public void signUpPressed()
-			{
-				if(loginKey.tryAcquire())
+				private SignUpMediator(Semaphore obj) {
+					loginKey=obj;	
+				}
+	
+				public SignUpMediator()
 				{
-					new Thread(new SignUpMediator(loginKey)).start();
 				}
-
-			}
-
-			private void tryToSignUp() throws Exception
-			{
-				boolean isOk = l.sendRequest(1);//1 = signup, 0 = login
-				if(!isOk)
+				
+				public void signUpPressed()
 				{
-					JOptionPane.showMessageDialog(null, "Username already exist.", "Choose a different username", JOptionPane.ERROR_MESSAGE);
+					if(loginKey.tryAcquire())
+					{
+						new Thread(new SignUpMediator(loginKey)).start();
+					}
+	
 				}
-			}
-			
-			@Override
-			public void run() {
-				try {
-					tryToSignUp();
-				} catch (Exception e1) {
-					System.out.println(e1.getMessage());
-				}
-				finally
+	
+				private void tryToSignUp() throws Exception
 				{
-					loginKey.release();
+					boolean isOk = l.sendRequest(1);//1 = signup, 0 = login
+					if(!isOk)
+					{
+						JOptionPane.showMessageDialog(null, "Username already exist.", "Choose a different username", JOptionPane.ERROR_MESSAGE);
+					}
 				}
-			}
+				
+				@Override
+				public void run() {
+					try {
+						tryToSignUp();
+					} catch (Exception e1) {
+						System.out.println(e1.getMessage());
+					}
+					finally
+					{
+						loginKey.release();
+					}
+				}
 		}
 		
 		
@@ -160,54 +158,50 @@ public class Mediator extends Thread{
 	{
 		OpenClassroom op;
 		
-		private class OpenClassMessagesMediator implements ActionListener
-		{
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(e.getActionCommand()==EditorFrame.SEND_CMD)
-				{
-					synchronized (op) { 
-						op.notifyAll();
+			private class OpenClassMessagesMediator implements ActionListener
+			{
+	
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if(e.getActionCommand()==EditorFrame.SEND_CMD)
+					{
+						synchronized (op) { 
+							op.notifyAll();
+						}
+						
 					}
-					
 				}
+				
 			}
-			
-		}
 		
-		public OpenClassMediator()
-		{
-			
-		}
-
-		@Override
-		public void run() {
-			EventQueue.invokeLater(new Runnable() {
+			public OpenClassMediator()
+			{
+				
+			}
+	
+			@Override
 			public void run() {
-					try {
+				EventQueue.invokeLater(new Runnable() {
+				public void run() {
+						try {
+	
+							startEditor();						
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}});
+			}
 
-						startEditor();						
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}});
-		}
-
-		public void startEditor()
-		{
-			OpenClassMessagesMediator msgMed = new OpenClassMessagesMediator();
-			EditorFrame frame = new EditorFrame(msgMed);
-			frame.setVisible(true);
-			op = new OpenClassroom(frame.ei);
-			Thread t1 = new Thread(op);
-			t1.start();
-
-		}
-
-		
-		
-		
+			public void startEditor()
+			{
+				OpenClassMessagesMediator msgMed = new OpenClassMessagesMediator();
+				EditorFrame frame = new EditorFrame(msgMed);
+				frame.setVisible(true);
+				op = new OpenClassroom(frame.data.ei);
+				Thread t1 = new Thread(op);
+				t1.start();
+	
+			}
 	}
 
 	public Mediator()
