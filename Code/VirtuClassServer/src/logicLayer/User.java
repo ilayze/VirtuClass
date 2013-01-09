3ac49dd79e2e80969a9da1436c6b7d2447739560
razco <rcm1_2@hotmail.com>
2013-01-09 17:54:08 +0200

    User class for holding user's socket and info

---
+++ Code/VirtuClassServer/src/logicLayer/User.java
@@ -0,0 +1,75 @@
+package logicLayer;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class User {
+
+	private String id="Anonymous";
+	private Socket socket;
+	private DataOutputStream os;
+	private DataInputStream is;
+	
+	public User(String id, Socket socket,DataOutputStream os,DataInputStream is)
+	{
+		if(id!=null)
+			this.id=id;
+		this.socket = socket;
+		this.os=os;
+		this.is=is;
+	}
+
+	public String getName() {
+		return id;
+	}
+
+	public Socket getSocketDescriptor() {
+		return socket;
+	}
+
+	public DataOutputStream getOutputStream() {
+		return os;
+	}
+
+	public DataInputStream getInputStream() {
+		return is;
+	}
+
+
+	public int quitChat() throws IOException{
+		int closedSum=0;//+1 for inputStream close, +2 for outputStream
+		if(is!=null)
+		{
+			is.close();
+			closedSum+=1;
+		}
+		if(os!=null)
+		{
+			os.close();
+			closedSum+=2;
+		}
+		if(socket!=null && !socket.isClosed())
+			socket.close();
+		
+		return closedSum;
+	}
+	
+	public boolean equals(Object obj)
+	{
+		if(obj instanceof User)
+		{
+			User usrToCompare = (User)obj;
+			if(usrToCompare.getName().equals(this.getName()))
+				return true;
+		}
+		return false;
+		
+	}
+	
+	
+	
+	
+	
+}
