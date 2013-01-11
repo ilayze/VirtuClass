8b53ff32de98a19332ac8da09285e7bd71d389b9
razco <rcm1_2@hotmail.com>
2013-01-11 12:53:02 +0300

    added new exception

---
+++ Code/VirtuClassServer/src/logicLayer/ClassroomNotDeletedSuccessfullyException.java
@@ -0,0 +1,13 @@
+package logicLayer;
+
+public class ClassroomNotDeletedSuccessfullyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2276884003376984483L;
+	public ClassroomNotDeletedSuccessfullyException() { super(); }
+	public ClassroomNotDeletedSuccessfullyException(String message) { super(message); }
+	public ClassroomNotDeletedSuccessfullyException(String message, Throwable cause) { super(message, cause); }
+	public ClassroomNotDeletedSuccessfullyException(Throwable cause) { super(cause); }
+}
