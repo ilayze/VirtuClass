c6fffd9d5a3b2b26a2ab5ae0163bc907d6b1c679
razco <rcm1_2@hotmail.com>
2013-01-14 19:53:36 +0200

    null object design pattern

---
+++ Code/VirtuClassClient/src/logicLayer/NullEditorInfo.java
@@ -0,0 +1,15 @@
+package logicLayer;
+
+public class NullEditorInfo implements EditorInfo {
+
+	@Override
+	public String getText() {
+		return "";
+	}
+
+	@Override
+	public void setText(String s) {
+		
+	}
+
+}
